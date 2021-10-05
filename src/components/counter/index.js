7fcbd7b6b0faabc50b1ecac1e5cf55fada847d7c
Abdoulaye <abdoulaye.guene92@gmail.com>
2021-10-05 12:05:01 +0000

    ✨feat(home:count) ouaga2018

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {FormattedMessage, injectIntl } from "gatsby-plugin-react-intl"
 import { ImportScript } from '../../services'
 
 const Counter = () => {
@@ -7,8 +7,20 @@ const Counter = () => {
   ImportScript("/js/load/counter.js");
 
   return (<section id="counter-part-2">
+          <div className="row justify-content-center">
+                <div className="col-lg-6">
+                    <div className="section-title text-center pb-15">
+                        <h3><FormattedMessage id="ouaga2018"/></h3>
+                        <div className="underline">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+        </div>
     <div className="counter-part-2 pt-80">
       <div className="container">
+
         <div className="row align-items-center">
           <div className="col-lg-3 col-sm-6">
             <div className="singel-counter text-center mt-40">
@@ -42,9 +54,9 @@ const Counter = () => {
         <div className="row justify-content-center">
           <div className="col-lg-8">
             <div className="video-2">
-              <img src="/images/video.jpg" alt="Video"/>
+              <img src="/images/ouaga2018.png" alt="Video"/>
                 <div className="content">
-                <a className="video-popup" href="https://www.youtube.com/watch?v=2uA4XOmsrrA" aria-label="button"><i className="flaticon-music-player-play"></i></a>
+                <a className="video-popup" href="https://www.youtube.com/playlist?list=PLQjNVx8Nv7w-GkhYtFN9DwqYcpovqcyu0" aria-label="button"><i className="flaticon-music-player-play"></i></a>
                 </div>
                         </div>
             </div>
@@ -53,5 +65,4 @@ const Counter = () => {
       </div>
     </section>
 )}
-
 export default Counter
