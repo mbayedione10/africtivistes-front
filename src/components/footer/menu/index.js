f97c21046c1808b7b978ef1ff4ab71dccdf06fb3
Abdoulaye <abdoulaye.guene92@gmail.com>
2021-10-22 11:05:25 +0000

    🌟Update (footer/menu)

--- src/components/footer/menu/index.js
+++ src/components/footer/menu/index.js
@@ -7,17 +7,14 @@ const Menu = () => (
   <div className="footer-link pt-45 pl-50">
     <div className="title mb-30">
       <h4>Menu</h4>
-    </div>
-          <div className="col-lg-11 order-last order-lg-first">
-            <nav className="navbar navbar-expand-lg">
-
+      <nav className="navbar navbar-expand-lg">
               <div className="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                   <Francais/>
                   <English/>
               </div>
             </nav>
-          </div>
-        </div>
+    </div>
+    </div>
 )
 
 export default Menu
