61bc4dde9dc522e6a40c24201c4cd4fabb080c98
Abdoulaye <abdoulaye.guene92@gmail.com>
2021-11-26 14:42:06 +0000

    ✨rubrique ACAP (contenu)

--- src/components/about/about2.js
+++ src/components/about/about2.js
@@ -1,25 +1,32 @@
 import React from 'react'
 
 const About2 = () => (
-  <section id="about-part-2" className="about-part-2 gray-bg pt-120 pb-130">
-    <div className="about-2-bg bg_cover d-none d-lg-block" style={{backgroundImage: `url(/images/acap-01.jpg`}}></div>
+  <section id="about-part-2" className="about-part-2 gray-bg pt-10 pb-20">
+    <div className="about-2-bg bg_cover d-none d-lg-block" style={{backgroundImage: `url(/images/prize.png`}}></div>
     <div className="container">
       <div className="row justify-content-end">
         <div className="col-lg-6">
           <div className="about-2-content">
-            <h3 className="mb-20"><span> #AfricTivistesPrize</span> : 337 nominations, 21 pays, 10 présélectionnés au bout</h3>
-            <p className="mb-20">43 jours ont suffi pour enregistrer 337 nominations venant de 21 pays du continent africain. Au bout, dix (10) personnes vont se disputer cinq (5) places pour Nairobi, au Kenya en octobre où ils seront distingués comme les jeunes acteurs de changement qui se sont fait le plus remarquer en Afrique. Toujours parmi eux, il sera désigné le champion citoyen, lauréat du AfricTivistesPrize 2021. C’est la personne dont le projet ou l'initiative est reconnue comme ayant eu le plus fort impact social.
-              <br/>
+            <h3 className="mb-20"><span> #AfricTivistesPrize : </span></h3>
+
+            <h6>Retrouvez toute l'actualité sur le Prix AfricTivistes pour l'Action Civique : </h6>
+            <p>AfricTivistes veut récompenser les jeunes modèles et acteurs de changement pour favoriser une émulation chez l’ensemble de la jeunesse africaine ...<a href="https://prize.africtivistes.org/fr/newsroom/africtivistes-civic-action-prize/">Lire la suite</a></p>            <br></br>
+
+              
+            <h6 className="mb-20">AfricTivistes Civic Action Prize 2021: Découvrez les noms des préfinalistes :</h6>
+            <p>Le processus de sélection des 10 préfinalistes du prix AfricTivistes pour l’Action Civique a été lancé le 16 juillet 2021, date de clôture des nominations...<a href="https://prize.africtivistes.org/fr/newsroom/africtivistes-civic-action-prize-2021-d%C3%A9couvrez-les-noms-des-pr%C3%A9finalistes/">Lire la suite</a></p>
+            <br></br>
+            
+            <h6 className="mb-20">Fin des nominations : </h6><p>43 jours ont suffi pour enregistrer 337 nominations venant de 21 pays du continent africain. Au bout, dix (10) personnes vont se disputer cinq (5) places pour Abidjan, en Côte d’Ivoire au mois de novembre où ils seront distingués comme les jeunes acteurs de changement qui se sont fait le plus remarquer en Afrique..<a href="https://prize.africtivistes.org/fr/newsroom/fin-des-nominations/">Lire plus</a></p>
+
+           
+
                 <br/>
-                Le potentiel de la jeunesse africaine est immense. Cela a été démontré du 03 juin au 16 juillet 2021, par le nombre impressionnant de profils et de projets proposés lors de l’appel à nominations pour le Prix AfricTivistes pour l’action civique. A la fin du décompte, il a été enregistré 337 nominations dans 21 pays. Il faut préciser que le concours était ouvert aux ressortissants des 54 pays africains âgés de 40 ans au plus. Et que ces derniers doivent avoir eu à réaliser des projets innovants à fort impact social ou communautaire. 
-                Il était convenu à l’issue de la présélection de retenir 5 profils qui seront mis en compétition en direction du 3ème Sommet AfricTivistes de Nairobi en Octobre 2021. Mais, vu l’engouement et le nombre consistant de candidatures, il a été décidé d’augmenter le nombre de présélectionnés à 10 profils afin de donner au maximum la chance aux porteurs d’initiatives d’être lauréat du Prix AfricTivistes.                  
-                </p>
-                <br/>
-                <a className="mb-20">En savoir plus
+                <a className="mb-20">En savoir plus</a>
                 <br/>
                   <a href="https://prize.africtivistes.org/fr/">https://prize.africtivistes.org/fr/ </a> <br/>
 
-                  Suivez-nous sur <span>Twitter</span> #AfricTivistesPrize</a>
+                  <a>Suivez-nous sur Twitter</a> <span>#AfricTivistesPrize</span> 
 
                 {/* <a href="/" className="main-btn">Support</a> */}
                     </div>
