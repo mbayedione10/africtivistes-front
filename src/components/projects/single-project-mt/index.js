d5fcd8d52af9bff67c52b016ed63d1519c5cfd87
Abdoulaye <abdoulaye.guene92@gmail.com>
2021-12-13 13:53:12 +0000

    update home page(Tous les articles)rendre visible le titre

--- src/components/projects/single-project-mt/index.js
+++ src/components/projects/single-project-mt/index.js
@@ -10,19 +10,23 @@ const SingleProjectMt = ({post}) => {
         
         <div className="col-lg-4 col-md-6" >
 
-        <div className="singel-project mt-50 text-center" >
-            <div className="image" >
-            <GatsbyImage image={image} alt={title} />
+        <div className="singel-news mt-50 " >
+            <div className="news-thumb" >
+
+                           <a href={link} ><GatsbyImage image={image} alt={title} /> </a>
+
             </div>
-            <div className="content"  style={{height: '100px'} , {width: '300px'}} >
-                <ul className="mb-15" >
-                    {/* <li><a className="project-popup" href={link}  aria-label="button" ><i className="flaticon-full-screen"></i></a></li> */}
+            <div className="content"  style={{height: '100px'} , {width: '360px'}} >
+                {/* <ul className="mb-15" >
+                    <li><a className="project-popup" href={link}  aria-label="button" ><i className="flaticon-full-screen"></i></a></li>
                     <li className="details"><a href={link}  aria-label="button"><i className="flaticon-right-arrow"></i></a></li>
-                </ul>
-                <span><a href={link} style={{backgroundColor:`#FEFEFE`}}><h6>{title.substring(0, 70)}...<br/>
-                <br/>{date}</h6></a></span><br/>
+                </ul> */}
+                <br/>{date}<br/>
+                <span><a href={link} ><h5 style={{color: '#383937'}}>{title.substring(0, 70)}...</h5></a></span><a href={link} style={{color: '#7f7f7f'}}> 
+                 < FormattedMessage id="readMore"/></a><br/><br/>
             </div>
         </div>
+        <br/>
         </div>
         
 
