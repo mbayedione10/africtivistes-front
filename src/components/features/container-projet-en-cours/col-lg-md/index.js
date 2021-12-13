007f9ade5c6d6bfc0d7230cf24b504bd64815f98
Abdoulaye <abdoulaye.guene92@gmail.com>
2021-12-13 13:57:22 +0000

    update components/features projet en cours

--- src/components/features/container-projet-en-cours/col-lg-md/index.js
+++ src/components/features/container-projet-en-cours/col-lg-md/index.js
@@ -5,17 +5,16 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 const ColLgMd = ({project}) => {
 
     return (
-        <div className="col-lg-4 col-md-6 col-sm-10">
-
+      <div className="col-lg-4 col-md-6 col-sm-10">
         <div className="singel-features-2 text-center mt-30">
-        <div className="icon pb-20">
-          {/* <i className="flaticon-global"></i> */}
-          <GatsbyImage image={getImage(project.featuredImage.node.localFile)} alt={project.featuredImage.node.altText} />
-        </div>
-        <div className="content pl-20">
-          <h4 className="mb-20">{project.title}</h4>
-          <p className="mb-15" dangerouslySetInnerHTML={{ __html: project.content.substring(0, 600)}}/>
-          <span className="details"><a href={project.link} ><FormattedMessage id="readMore"/> <i className="flaticon-right-arrow"></i></a></span>
+          <div className="icon pb-20 singel-news ">
+            {/* <i className="flaticon-global"></i> */}
+            <GatsbyImage image={getImage(project.featuredImage.node.localFile)} alt={project.featuredImage.node.altText} />
+          </div>
+          <div className="content pl-20">
+            <h4 className="mb-20">{project.title}</h4>
+            <p className="mb-15" dangerouslySetInnerHTML={{ __html: project.content.substring(0, 600)}}/>
+            <span className="details"><a href={project.link} ><FormattedMessage id="readMore"/> <i className="flaticon-right-arrow"></i></a></span>
         </div>
       </div>
       </div>
