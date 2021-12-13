d450d7fbc5e86d1e4b84582cc2f505853596ba8c
Abdoulaye <abdoulaye.guene92@gmail.com>
2021-12-13 13:50:11 +0000

    update page equipe

--- src/pages/africtivistes/equipe.js
+++ src/pages/africtivistes/equipe.js
@@ -1,15 +1,19 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-
+import PageBanner from "../../components/pageBanner"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
+import Team from '../../components/team'
+import CallAction2 from '../../components/callAction/callactions2'
 
 const EquipePage = ({data}) => {
-const post = data.allWpPage.nodes[0]
-const link = post.translations ? post.translations[0].link : ''
+const {post, title, translations} = data.allWpPage.nodes[0]
+const link = translations ? translations[0].link : ''
 return(<Layout translation={link}>
-  <Seo title="L'équipe" />
-    <h1>{post.title}</h1>
+    <Seo title="L'équipe" />
+    <PageBanner title={title} />
+    <Team teams={data.allWpTeam.nodes}/>
+    <CallAction2/>
   </Layout>
 )}
 export default EquipePage
@@ -20,6 +24,31 @@ export const query = graphql`
       title
       translations {
         link
+      }
+    }
+  }
+  allWpTeam {
+    nodes {
+      title
+      excerpt
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(
+                width: 89,
+                placeholder: DOMINANT_COLOR
+              )
+            }
+          }
+        }
+      }
+      social {
+        facebook
+        instagram
+        linkedin
+        twitter
       }
     }
   }
