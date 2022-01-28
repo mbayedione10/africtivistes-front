93a6fbd0577128a32ecb18c4bf8f6ea768d25fb6
Abdoulaye <abdoulaye.guene92@gmail.com>
2022-01-28 20:57:15 +0000

    ✨ update pageBanner programs

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -1,10 +1,9 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import { IntlContextConsumer, injectIntl } from "gatsby-plugin-react-intl"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
+import { FormattedMessage } from 'gatsby-plugin-react-intl'
 import PageBanner from '../components/pageBanner'
 import ProjectsPage from '../components/projects/page'
 import Counter2 from '../components/counter/counter2'
@@ -20,7 +19,24 @@ const ProgrammesPage = ({ data, intl }) => {
     {({ language: currentLocale }) =>
         currentLocale === 'en' && <Layout translation={link}>
           <Seo lang={intl.locale} title={title} />
-          <PageBanner title={title} />
+          {/* <PageBanner title={title} /> */}
+          <section id="page-banner" className="bg_cover pt-200 pb-200" style={{ backgroundImage: `url(/images/titre.png`}} data-overlay="6">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="page-banner-content text-center">
+                        <h2>{title}</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb justify-content-center">
+                                <li className="breadcrumb-item"><a href="/" aria-label="button"><FormattedMessage id="home" /></a></li>
+                                <li className="breadcrumb-item active" aria-current="page">{title}</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
           <ProjectsPage programmes={data.allWpProgrammeType.nodes} projects={data.allWpProgramme.nodes} />
           <Counter2 />
           <CallAction2 />
