584573471a03a508d8e9bcd33a66283091938c17
Abdoulaye <abdoulaye.guene92@gmail.com>
2022-01-28 21:00:08 +0000

    ✨update pageBanner AfricTivistes

--- src/pages/africtivistes/index.js
+++ src/pages/africtivistes/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-
+import { FormattedMessage } from 'gatsby-plugin-react-intl'
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import PageBanner from "../../components/pageBanner"
@@ -16,7 +16,24 @@ const AfrictivistesPage = ({data}) => {
   const link = translations ? translations[0].link : ''
   return (<Layout translation={link}>
       <Seo title="À propos de nous" />
-      <PageBanner title={title} />
+      {/* <PageBanner title={title} /> */}
+      <section id="page-banner" className="bg_cover pt-200 pb-200" style={{ backgroundImage: `url(/images/logo_africtivistes.png`}} data-overlay="6">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="page-banner-content text-center">
+                        <h2>{title}</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb justify-content-center">
+                                <li className="breadcrumb-item"><a href="/" aria-label="button"><FormattedMessage id="home" /></a></li>
+                                <li className="breadcrumb-item active" aria-current="page">{title}</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
       <About3 content={content} picture={featuredImage.node.localFile}/>
       <Intervention/>
       <Team teams={data.allWpTeam.nodes}/>
--- src/pages/africtivistes/valeurs.js
+++ src/pages/africtivistes/valeurs.js
@@ -4,14 +4,27 @@ import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import PageBanner from "../../components/pageBanner"
 import Values from "../../components/values"
-
+import { FormattedMessage } from 'gatsby-plugin-react-intl'
 
 const ValeursPage = ({data}) => {
   const { title, translations} = data.allWpPage.nodes[0]
   const link = translations ? translations[0].link : ''
   return (<Layout translation={link}>
       <Seo title="À propos de nous" />
-      <PageBanner title={title} />
+      {/* <PageBanner title={title} /> */}
+      <section id="page-banner" className="bg_cover pt-200 pb-200" style={{ backgroundImage: `url(/images/nosvaleurs.png`}} data-overlay="6">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="page-banner-content text-center">
+                        <nav aria-label="breadcrumb">
+                            
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
       <Values posts={data.allWpPage.edges}/>
     </Layout>)
 }
