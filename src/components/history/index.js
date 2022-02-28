233e1c5253ca29a1f3995b560ffd9750cbb5e76b
Abdoulaye <abdoulaye.guene92@gmail.com>
2022-02-28 09:54:27 +0000

    ✨Update content page histoire

--- src/components/history/index.js
+++ src/components/history/index.js
@@ -1,28 +1,39 @@
 import React from 'react'
 
 const History = () => (
-  <section id="company-history" class="pt-80 pb-50">
+<section id="company-history" class="pt-30 pb-30">
         <div class="container">
+        <div className="row justify-content-center">
+                <div className="col-lg-6">
+                    <div className="section-title text-center pb-15">
+                        <h2>Présentation de AfricTivistes</h2>
+                        <div className="underline">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+        </div>
             <div class="row">
                 <div class="col-lg-6">
                     <div class="years-experience mt-45">
-                        <h3 class="mb-25">25 Year’s <br/> Of Experience</h3>
-                        <p class="mb-10">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis natus error sit voluptatem accusantium doloremque laud antium totam remaperiam.</p>
-                        <ul>
+                        <h3 class="mb-25">Réseau des web-activistes<br/>Pro-démocratie</h3>
+                        <p class="mb-10">La ligue africaine des cyber-activistes et blogueurs pour la démocratie, communément appelée AfricTivistes, est l’union des blogueurs et web-activistes du continent et de sa diaspora pour promouvoir et défendre les valeurs démocratiques, les droits humains et la bonne gouvernance à travers le numérique. AfricTivistes a été créée en novembre 2013 en vue de mettre en réseau tous les acteurs de changement en Afrique afin d’apporter des solutions concrètes et idoines aux problèmes majeurs qui freinent le développement et l’unité du continent.</p>
+                        {/* <ul>
                             <li>Lobortis massa fringilla odio.</li>
                             <li>Convallis ligula ligula gravida tristique.</li>
                             <li>Occaecat cupidatat non proident</li>
-                        </ul>
+                        </ul> */}
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="history-image mt-50">
-                        <img src="/images/company-history/ch-1.jpg" alt="company history"/>
+                        <img src="/images/logo2.png" alt="company history"/>
                     </div>
                 </div>
             </div>
-            <div class="our-history pt-110">
-                <div class="row justify-content-center">
+            <div class="our-history pt-30">
+                {/* <div class="row justify-content-center">
                     <div class="col-lg-6">
                         <div class="section-title text-center">
                             <h3>Our History</h3>
@@ -32,54 +43,65 @@ const History = () => (
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="our-single-history pb-80">
+                </div> */}
+                <div class="our-single-history pb-30">
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="history-image mt-50">
-                                <img src="/images/company-history/ch-2.jpg" alt="company history"/>
+                                <img src="/images/histoire.png" alt="company history"/>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="our-history-content mt-50">
-                                <span class="mb-10">25 July, 2018</span>
-                                <h4 class="mb-15">1995 - Company Started</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. <br/> <br/> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                {/* <span class="mb-10">25 July, 2018</span> */}
+                                <h4 class="mb-15">Le numérique comme outil de transformation civique, sociale et politique</h4>
+                                <p>AfricTivistes, en tant qu’organisation dynamique élargie à l’ensemble des pays africains, se focalise sur la Consolidation de la démocratie, non seulement à l'échelle de chaque pays mais aussi et surtout à l’échelle continentale. Nous entendons par consolidation de la démocratie, toute action tendant vers une démocratie réelle, une démocratie participative, une cyberdémocratie, une E-gouvernance et l’ancrage effective de la culture démocratique dans nos pays respectifs.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="our-single-history pb-80">
+                <div class="our-single-history pb-30">
                     <div class="row">
                         <div class="col-lg-6 order-last order-lg-first">
                             <div class="our-history-content mt-50">
-                                <span class="mb-10">25 July, 2018</span>
-                                <h4 class="mb-15">1995 - Company Started</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. <br/> <br/> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                {/* <span class="mb-10">25 July, 2018</span> */}
+                                <h4 class="mb-15">Agir pour le changement (Act for social change)</h4>
+                                <p>AfricTivistes est résolue à intervenir directement ou indirectement dans les différents processus de changement politique et social. Elle mène et pilote des projets de développement et d'implication citoyenne dans les dispositifs démocratiques. Elle développe des stratégies visant à veiller et à faire surveiller les dirigeants politiques dans leurs actions et dans la réalisation de leurs promesses pour inciter à la transparence et à la bonne gouvernance. Elle est une vitrine de la citoyenneté augmentée en Afrique.<br/></p>
                             </div>
                         </div>
                         <div class="col-lg-6 order-lg-last order-first">
                             <div class="history-image mt-50">
-                                <img src="/images/company-history/ch-3.jpg" alt="company history"/>
-                                <div class="history-video">
+                                <img src="/images/logo2.png" alt="company history"/>
+                                {/* <div class="history-video">
                                     <a class="video-popup" href="# " aria-label="link"><i class="flaticon-music-player-play"></i></a>
-                                </div>
+                                </div> */}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="our-single-history pb-80">
+                <div class="our-single-history pb-30">
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="history-image mt-50">
-                                <img src="/images/company-history/ch-4.jpg" alt="company history"/>
+                                <img src="/images/histoire.png" alt="company history"/>
                             </div>
                         </div>
                         <div class="col-lg-6">
-                            <div class="our-history-content mt-50">
-                                <span class="mb-10">25 July, 2018</span>
-                                <h4 class="mb-15">1995 - Company Started</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. <br/> <br/> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <div class="years-experience mt-45">
+                                {/* <span class="mb-10">25 July, 2018</span> */}
+                                <h4 class="mb-15">Réseau d’influence, de plaidoyer, de capacitation et d’actions à fort impact social et politique</h4>
+                                <p>L’organisation est aujourd’hui le réseau de jeunes le plus influent en Afrique francophone, en tant que force de contestation (politiques publiques et gouvernance), force de proposition (solutions innovantes, nouvelles dynamiques citoyennes, projets et approches inclusives) et force de contribution (programmes de capacitation, co-construction et collaboration pour une politique de gouvernance ouverte). 
+AfricTivistes est composée de plusieurs profils dont en majorité des acteurs de l’information de la société civile (blogueurs, influenceurs digitaux, journalistes citoyens, …), des activistes, des informaticiens, des journalistes, des experts en données ouvertes, des experts formateurs aux technologies numériques, des juristes, des réseaux et mouvements citoyens, etc.
+Ce fort réseau nous a permis de développer plusieurs pôles de compétences : </p><br></br>
+                        <ul>
+                            <li>Construction et déploiement d’une dynamique communautaire;</li>
+                            <li>Conception et intégration de projets à forte valeur ajoutée orientée jeunes, leadership, engagement citoyen et transformation social;</li>
+                            <li>Capacité de mobilisation sur des sujets d’intérêt public;</li>
+                            <li>Forte influence dans le cyberespace;</li>
+                            <li>Capacitation en gestion de projets numériques, sécurité informatique, open data, solution médias, citoyenneté augmentée, …</li>
+                            <li>Promotion et développement de contenus numériques;</li>
+                            <li>Développement et déploiement de solutions informatiques pour renforcer la liberté d’expression et l’accès à l’information (VPN, réseau social privé, radio pirate Cloud, serveur, …) </li>
+                        </ul>
                             </div>
                         </div>
                     </div>
