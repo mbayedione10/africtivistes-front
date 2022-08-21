355d53cb3bd5f8cd1cda67c2c7e769f329d4488a
Abdoulaye <abdoulaye.guene92@gmail.com>
2022-08-21 19:53:52 +0000

    ♻️feat create page EN (Rubrique Nos communautes)

--- src/pages/africtivistes-en/our-communities.js
+++ src/pages/africtivistes-en/our-communities.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import * as React from "react"
 import { graphql } from "gatsby"
-import { IntlContextConsumer } from "gatsby-plugin-react-intl"
+import { IntlContextConsumer, injectIntl } from "gatsby-plugin-react-intl"
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+import { FormattedMessage } from 'gatsby-plugin-react-intl'
+import PageCommunaute from "../../components/projects/pagecommunaute"
 
-import CommunautesPage from '../africtivistes/nos-communautes'
-
-const Communities = ({ data }) => (
-  <IntlContextConsumer>
-    {({ language: currentLocale }) => 
-      currentLocale === 'en' && <CommunautesPage data={data} />
+const Communities = ({ data }) => {
+  const { title, translations } = data.allWpPage.nodes[0]
+  const link = translations ? translations[0].link : ''
+  return (
+    <IntlContextConsumer>
+    {({ language: currentLocale }) =>
+      currentLocale === 'en' && <Layout translation={link}>
+      <Seo title={title} />
+      {/* <PageBanner title={title} /> */}
+      <section id="page-banner" className="bg_cover pt-200 pb-200" style={{ backgroundImage: `url(/images/communautess.jpeg`}} data-overlay="6">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="page-banner-content text-center">
+                        <h2>{title}</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb justify-content-center">
+                                <li className="breadcrumb-item"><a href="/" aria-label="button"><FormattedMessage id="home" /></a></li>
+                                <li className="breadcrumb-item active" aria-current="page">{title}</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+      <PageCommunaute programmes={data.allWpCommunauteType.nodes} projects={data.allWpCommunautes.nodes} />
+      {/* <Counter2/>
+      <CallAction2/> */}
+    </Layout>
     }
-  </IntlContextConsumer>
-)
-
+    </IntlContextConsumer>
+  )}
+export default Communities
 export const query = graphql`
   query {
   allWpPage(filter: {slug: {eq: "our-communities"}}) {
@@ -22,10 +50,27 @@ export const query = graphql`
       }
     }
   }
-  allWpTeam {
+  allWpCommunauteType(
+    filter: {language: {code: {eq: FR}}}
+
+  ) {
     nodes {
+      id
+      name
+      slug
+    }
+  }
+  allWpCommunautes(filter: {language: {code: {eq: FR}}}) {
+    nodes {
+      id
       title
-      excerpt
+      content
+      link
+      communauteTypes {
+        nodes {
+          slug
+        }
+      }
       featuredImage {
         node {
           altText
@@ -33,18 +78,19 @@ export const query = graphql`
             childImageSharp {
               gatsbyImageData(
                 width: 89,
-                placeholder: DOMINANT_COLOR
-              )
+                height: 89, 
+              placeholder: DOMINANT_COLOR)
             }
           }
         }
       }
-      social {
+      communaute {
         facebook
         instagram
         linkedin
         twitter
       }
+      
     }
   }
   contact: allWpPage(filter: {slug: {eq: "contact-us"}}) {
@@ -73,5 +119,3 @@ export const query = graphql`
   }
 }
 `
-
-export default Communities
