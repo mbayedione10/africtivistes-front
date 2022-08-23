40579bf1a64400ee8fd0e315e47411f04faaaaf7
Abdoulaye <abdoulaye.guene92@gmail.com>
2022-08-23 12:08:10 +0000

    ✨feat (page EN) Content | Terms and conditions  #16

--- src/pages/terms-condition.js
+++ src/pages/terms-condition.js
@@ -12,14 +12,59 @@ export default TermsCondition
 export const query = graphql`
   query {
     allWpPage(filter: {slug: {eq: "terms"}, language: {code: {eq: EN}}}) {
-        nodes {
+      nodes {
           title
-          content
-          link
           translations {
             link
           }
         }
+        edges {
+          node {
+            id
+            date(locale: "")
+            content
+            link
+            title
+            featuredImage {
+                node {
+                  altText
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData(
+                        width: 550,
+                        height: 300,
+                        placeholder: DOMINANT_COLOR
+                      )
+                    }
+                  }
+                }
+          }
+        }
+        }
+    }
+    contact: allWpPage(filter: {slug: {eq: "nous-contacter"}}) {
+    nodes {
+      title
+      content
+      slug
+      link
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(
+                width: 555,
+                placeholder: DOMINANT_COLOR
+              )
+            }
+          }
+        }
       }
+      translations {
+        link
+      }
+    }
+  }  
 }
 `
