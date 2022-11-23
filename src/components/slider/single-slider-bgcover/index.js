7a389c543d3693717b7f5f3b62156ef2c8a89d8a
mbayedione10 <mbayedione10@gmail.com>
2022-11-23 13:39:18 +0000

    👷Dev I Home | Redirection des liens de la page d'accueil

--- src/components/slider/single-slider-bgcover/index.js
+++ src/components/slider/single-slider-bgcover/index.js
@@ -3,8 +3,9 @@ import {FormattedMessage, injectIntl } from "gatsby-plugin-react-intl"
 import { Link } from "gatsby"
 
 const SingleSliderBgcover = ({post,contacts}) => {
-    const{title, link,excerpt, featuredImage}=post.node
+    const{title, link,excerpt, featuredImage, categories}=post.node
     const image = featuredImage && featuredImage.node.localFile.childImageSharp.gatsbyImageData.images.fallback.src
+    const path= `${categories.nodes[0].slug}${link}`
 
     return (
         <section id="page-banner" className="single-slider bg_cover d-flex align-items-center" style={{backgroundImage:`url(${image})`}} data-overlay="8">
@@ -12,9 +13,9 @@ const SingleSliderBgcover = ({post,contacts}) => {
             <div className="row">
                 <div className="col-lg-5">
                     <div className="page-banner-content">
-                        <Link to={link}><h3 className="mb-5" style={{color:'#943126'}} data-animation="bounceInLeft" data-delay="1s" ><span>{title}...</span></h3></Link>
+                        <Link to={path}><h3 className="mb-5" style={{color:'#943126'}} data-animation="bounceInLeft" data-delay="1s" ><span>{title}...</span></h3></Link>
                         <p className="mb-5" style={{color:'white'}} data-delay="1.5s" dangerouslySetInnerHTML={{ __html: excerpt }}/>
-                        <a  className="main-btn" data-animation="fadeInUp" data-delay="1.5s" href={link}><FormattedMessage id ="suite"/></a>
+                        <a  className="main-btn" data-animation="fadeInUp" data-delay="1.5s" href={path}><FormattedMessage id ="suite"/></a>
                     </div>
                 </div>
             </div>
