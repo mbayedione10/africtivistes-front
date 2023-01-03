33a2191525faacd41742554640f150ae8da5a043
mbayedione10 <mbayedione10@gmail.com>
2023-01-03 15:41:42 +0000

    ♻️ Refactor | Home: Add style on "programme en cours"

--- src/components/features/container-projet-en-cours/col-lg-md/index.js
+++ src/components/features/container-projet-en-cours/col-lg-md/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import {FormattedMessage, injectIntl } from "gatsby-plugin-react-intl"
+import { FormattedMessage, injectIntl } from "gatsby-plugin-react-intl"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const ColLgMd = ({project}) => {
+const ColLgMd = ({ project }) => {
 
-    return (
-      <div className="card text-center">
-        <div className="card-img-top" >
-            <a href={project.link}> <GatsbyImage  image={getImage(project.featuredImage.node.localFile)} alt={project.featuredImage.node.altText} /></a>
-        </div>
-        <div class="card-body">
-          <a href={project.link}><h4>{project.title}</h4></a>
-        </div>
-        </div>
-    )
+  return (
+    <div className="card text-center">
+      <div className="card-img-top" >
+        <a href={project.link}> <GatsbyImage image={getImage(project.featuredImage.node.localFile)} alt={project.featuredImage.node.altText} /></a>
+      </div>
+      <div class="card-body" style={{
+        backgroundColor: '#a63117'
+      }}>
+        <a href={project.link}><h4 style={{ color: '#fff' }}>{project.title}</h4></a>
+      </div>
+    </div>
+  )
 }
 
 export default injectIntl(ColLgMd)
