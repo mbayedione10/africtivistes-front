0c03c5a6f72d7104ebce22b391b5d7af6ed907f4
dofbi.eth <genova@dofbi.com>
2023-10-28 16:13:04 +0000

    ✨ feat (hook): query recent posts

---
+++ src/hooks/query/recentPosts.js
@@ -0,0 +1,41 @@
+import { useStaticQuery, graphql } from "gatsby"
+
+export const useRenctPosts = () => {
+  const {allWpPost} = useStaticQuery(
+    graphql`
+    query recentPost {
+        allWpPost(
+          limit: 15
+          sort: {fields: date, order: DESC}
+          filter: {language: {code: {eq: FR}}}
+        ) {
+          edges {
+            node {
+              id
+              title
+              date(formatString: "DD MMMM, YYYY", locale: "fr")
+              excerpt
+              link
+              featuredImage {
+                node {
+                  altText
+                  big: localFile {
+                    childImageSharp {
+                      gatsbyImageData(width: 750, height: 360, placeholder: DOMINANT_COLOR)
+                    }
+                  }
+                  small: localFile {
+                    childImageSharp {
+                      gatsbyImageData(width: 70, height: 68, placeholder: DOMINANT_COLOR)
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  return allWpPost.edges
+}
