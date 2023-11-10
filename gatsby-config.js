7199a0bd74b96c6f9a9803f6f15dbd3a7df9daf7
dofbi.eth <genova@dofbi.com>
2023-11-10 12:20:56 +0000

    👷 chore (package): remove gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,9 +92,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-react-intl`,
