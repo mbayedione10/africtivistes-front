2b1b785afc62e9cca9987357377f05680e40d8fd
Abdoulaye Guene <abdoulaye.guene92@gmail.com>
2024-02-26 13:33:57 +0000

    Refacrtor rubrique plateformes

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -3,9 +3,11 @@ import ContainerPEC from './container-projet-en-cours'
 
 const Features = ({projects}) => (
   <section id="features-part-2" className="featuera-2 pt-30 pb-30" text-align="center">
+    <div id="counter-part-3" className="bg_cover pt-80 pb-120" style={{backgroundImage:`url(/images/slider/backgroundplateforme.png)`}}>
 
-    <div className="container">
-        <ContainerPEC projects={projects}/>
+    <div className="container" style={{ maxWidth: '1500px' }}>
+  <ContainerPEC projects={projects}/>
+</div>
     </div>
   </section>
 )
