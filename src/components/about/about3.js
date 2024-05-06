6e0d2d6a0fb5eea3d35032f2a22e37b8f2692638
Abdoulaye Guene <36207283+Abdoulayeguene@users.noreply.github.com>
2024-05-06 14:16:56 +0000

    refactor page qui sommes nous

--- src/components/about/about3.js
+++ src/components/about/about3.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { FormattedMessage } from 'gatsby-plugin-react-intl'
+import React from 'react';
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-const About3 = ({ content, picture}) => {
-  
-  const image = getImage(picture)
+const About3 = ({ content, valeurs, histoire, picture, language }) => {
+  const image = getImage(picture);
 
-  return(<section id="company-about" className="pt-30 pb-30">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-6">
-          <div className="company-about-cont pt-45" dangerouslySetInnerHTML={{ __html: content }} />
+  // Vérifier la langue et définir les liens en conséquence
+
+  return (
+    <section id="company-about" className="pt-30 pb-30">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="company-about-cont pt-45" dangerouslySetInnerHTML={{ __html: content }} />
           </div>
           <div className="col-lg-6 pt-50">
             <div className="company-about-image">
-            <GatsbyImage image={image} alt={picture.altText}/>
-            <div class="years-experience mt-45">
-              <ul>
-                <li><a href="/"><h3><FormattedMessage id='nosValeurs'/></h3></a></li>
-                <li><a href="/"><h3><FormattedMessage id='notreHistoire'/></h3></a></li>
-              </ul>
+              <GatsbyImage image={image} alt={picture.altText} />
+              <div className="years-experience mt-45">
+                <ul>
+                  {valeurs.map((value, index) => (
+                    <li key={index}><a href={value.link}><h3>Nos valeurs</h3></a></li>
+                  ))}
+                  {histoire.map((story, index) => (
+                    <li key={index}><a href={story.link}><h3>Notre Histoire</h3></a></li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>)}
+    </section>
+  );
+}
 
-export default About3
+export default About3;
