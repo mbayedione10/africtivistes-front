7dd02e880530b16dd3ea609adbc0ba7356e791dc
Abdoulaye Guene <36207283+Abdoulayeguene@users.noreply.github.com>
2024-10-23 13:55:26 +0000

    mise a jour des blogs pour laffichage

--- src/components/features/container-projet-en-cours/col-lg-md/index.js
+++ src/components/features/container-projet-en-cours/col-lg-md/index.js
@@ -5,7 +5,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 const ColLgMd = ({ project }) => {
   return (
     <div className="card rounded-0 h-100 blog-card-">
-      <div className="card-img-wrapper">
+      <div className="card-img">
         <a href={project.plateforme.url} target="_blank" rel="noopener noreferrer">
           <GatsbyImage 
             image={getImage(project.featuredImage.node.localFile)} 
