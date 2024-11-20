a5dfc51c3e583c1c3018bd9cbb613cda385be884
Abdoulaye Guene <36207283+Abdoulayeguene@users.noreply.github.com>
2024-11-20 16:11:09 +0000

    update page accueil & actualite

--- src/components/blog-sidebar-communiques/index.js
+++ src/components/blog-sidebar-communiques/index.js
@@ -3,7 +3,7 @@ import SingleBlog from './single-blog'
 
 const BlogSidebar = ({ posts, numPages, currentPage}) => {
     return (
-        <section id="blog-grid" className="">
+        <section id="latest-news" className="pt-30 pb-30">
         <div className="container">
                     <SingleBlog posts={posts} numPages={numPages} currentPage={currentPage} />
         </div>
